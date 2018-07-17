258abb1dba5969d6a193432f5f3fe4518775939d
davecastaneda <smopo24@gmail.com>
2018-07-17 01:00:38 -0500

    Added a route and some comments

---
+++ sinatra.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+
+# Default file paths
+# set :root, File.dirname(__FILE__)
+# set :public_folder, settings.root + "public"
+# set :views, settings.root + "views"
+
+
+get '/' do
+    "Explore California"
+end
