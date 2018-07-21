d1a113e00137a57f97eb511a7d7e7e602c0431ff
davecastaneda <smopo24@gmail.com>
2018-07-21 14:24:47 -0500

    Moved some files, updated routes and symbols

--- sinatra.rb
+++ sinatra.rb
@@ -11,33 +11,48 @@ get '/' do
 end
 
 get '/contact' do
-  @page_title = "Welcome to Explore California!"
+  @page_title = "Explore California: Contact Us"
   erb :contact
 end
 
 get '/explorers' do
-  @page_title = "Welcome to Explore California!"
+  @page_title = "Explore California: Explorers"
   erb :explorers
 end
 
 get '/mission' do
-  @page_title = "Welcome to Explore California!"
+  @page_title = "Explore California: Mission"
   erb :mission
 end
 
 get '/resources' do
-  @page_title = "Welcome to Explore California!"
+  @page_title = "Explore California: Resources"
   erb :resources
 end
 
+get '/resources/faq' do
+  @page_title = "Explore California: faq"
+  erb "resources/faq".to_sym
+end
+
 get '/support' do
-  @page_title = "Welcome to Explore California!"
+  @page_title = "Explore California: Support"
   erb :support
 end
 
 get '/tours' do
-  @page_title = "Welcome to Explore California!"
+  @page_title = "Explore California: Tours"
   erb :tours
+end
+
+get '/tours/tour_detail_backpack' do
+  @page_title = "Explore California: Tour Detail - Backpack"
+  erb "tours/tour_detail_backpack".to_sym
+end
+
+get '/tours/tour_tour_detail_bigsur' do
+  @page_title = "Explore California: Tour Detail - Big Sur"
+  erb "tours/tour_tour_detail_bigsur".to_sym
 end
 
 get '/test' do
