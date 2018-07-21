634a875cf4fa7498ce5ad1f4787bc2d40a5d36b6
davecastaneda <smopo24@gmail.com>
2018-07-20 23:07:12 -0500

    Changed title

--- sinatra.rb
+++ sinatra.rb
@@ -6,6 +6,7 @@ require 'sinatra'
 # set :views, settings.root + "views"
 
 get '/' do
+  @page_title = "Welcome to Explore California!"
   erb :index
 end
 
@@ -13,4 +14,3 @@ get '/test' do
   template = 'The current year is <%= Time.now.year %>.'
   erb template
 end
-
