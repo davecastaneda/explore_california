ad31cc147c702240423949792f9818a5f970d4b0
davecastaneda <smopo24@gmail.com>
2018-07-21 02:18:51 -0500

    Added page title to templates

--- sinatra.rb
+++ sinatra.rb
@@ -11,26 +11,32 @@ get '/' do
 end
 
 get '/contact' do
+  @page_title = "Welcome to Explore California!"
   erb :contact
 end
 
 get '/explorers' do
+  @page_title = "Welcome to Explore California!"
   erb :explorers
 end
 
 get '/mission' do
+  @page_title = "Welcome to Explore California!"
   erb :mission
 end
 
 get '/resources' do
+  @page_title = "Welcome to Explore California!"
   erb :resources
 end
 
 get '/support' do
+  @page_title = "Welcome to Explore California!"
   erb :support
 end
 
 get '/tours' do
+  @page_title = "Welcome to Explore California!"
   erb :tours
 end
 
