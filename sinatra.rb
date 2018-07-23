c6d1c48f6f158f2595b609638227b891d6b6eed1
davecastaneda <smopo24@gmail.com>
2018-07-23 00:32:24 -0500

    Moved helpers into a seperate file

--- sinatra.rb
+++ sinatra.rb
@@ -5,21 +5,7 @@ require 'sinatra'
 # set :public_folder, settings.root + "public"
 # set :views, settings.root + "views"
 
-helpers do
-
-  def render_view(path, options={})
-    erb(path.to_sym, options)
-  end
-
-  def render_partial(path, options={})
-    parts = path.to_s.split('/')
-    filename = parts.pop
-    newpath = [parts, "_#{filename}"].join('/')
-    erb(newpath.to_sym, options.merge(:layout => false))
-  end
-end
-
-
+require_relative('helpers.rb')
 
 get '/' do
   @page_title = "Welcome to Explore California!"
